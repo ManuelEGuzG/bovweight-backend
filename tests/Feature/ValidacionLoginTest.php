f9db336b681c3308d8b95ba13935b1760f66e2f0
TatianaJSM <tatiana.jimenezsolis@ucr.ac.cr>
2026-06-19 10:37:12 -0600

    Agrega pruebas de validacion para login

--- tests/Feature/ValidacionLoginTest.php
+++ tests/Feature/ValidacionLoginTest.php
@@ -23,4 +23,14 @@ class ValidacionLoginTest extends TestCase
 
         $response->assertStatus(422);
     }
+
+    public function test_login_falla_con_correo_invalido()
+    {
+        $response = $this->postJson('/api/v1/auth/login', [
+            'correo' => 'correo-invalido',
+            'contrasena' => 'password'
+        ]);
+
+        $response->assertStatus(422);
+    }
 }
